0888e85572bc04e14a562ff11b5778e5eaa995e9
timod94 <dorstt94@gmail.com>
2024-10-17 15:34:02 +0200

    update port in login and register page & setup the server to start using 'npm start'

--- frontend/src/pages/Register.js
+++ frontend/src/pages/Register.js
@@ -42,7 +42,7 @@ function Register() {
 
 try {
 
-  const response = await fetch(`http://localhost:3000/register`, {
+  const response = await fetch(`http://localhost:5000/register`, {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json'
