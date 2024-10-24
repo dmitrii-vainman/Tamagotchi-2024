404c0c26c20406f2e88544d4b1e01eeff01441aa
jihenmich <jihen.michalski@outlook.com>
2024-10-24 10:51:11 +0200

    Route-login

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -8,7 +8,7 @@ import MainPage from "./pages/MainPage.jsx"
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import RequestReset from './pages/requestPassword.js';
 import ResetPassword from './pages/resetPassword.js';
-
+import ProtectedComponent from './components/ProtectedComponent.js'
 
 
 function App() {
@@ -30,6 +30,7 @@ function App() {
           <Route path='/reset-password' element={<ResetPassword />} />
           <Route path='/reset' element={<RequestReset />} />
           <Route path='/*' element={<NotFound />} />
+          <Route path="/protected" element={<ProtectedComponent />} />
         </Routes>
       </div>
     </Router>
